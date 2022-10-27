6cc923e3268b22588af9dd28680f90614239d0c5
Fi <vancetti@googlemail.com>
2022-10-27 14:50:05 +0200

    first commit

---
+++ kupferbiene/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <h href="/">Kupferbiene</h>
+      </header>
+      <main>
+        <p>list of products</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
